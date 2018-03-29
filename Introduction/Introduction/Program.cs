42fad2db5599bb7d5d18c4cb190e9d78124c2e64
carson <carsonjamescook@gmail.com>
2018-03-28 19:18:46 -0700

    Playing around with classes.

--- Introduction/Introduction/Program.cs
+++ Introduction/Introduction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Introduction
 {
@@ -6,7 +7,39 @@ namespace Introduction
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car1, car2;
+
+            car1 = new Car("Red");
+            Console.WriteLine(car1.Describe());
+
+            car2 = new Car("Green");
+            Console.WriteLine(car2.Describe());
+        }
+    }
+
+    class Car
+    {
+        private string color;
+        private string [] acceptableColors = {"Red", "Green", "Blue",
+            "Silver", "Black"};
+
+        public Car(string color)
+        {
+            if (acceptableColors.Contains(color))
+                this.color = color;
+            else
+                Console.WriteLine("Illegal Color");
+        }
+
+        public string Describe()
+        {
+            return "This car is " + Color;
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
         }
     }
 }
