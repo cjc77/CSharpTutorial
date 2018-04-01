7e397dd9a34dcc75af3b159f6a4468d015c96182
carson <carsonjamescook@gmail.com>
2018-04-01 00:56:30 -0700

    Done with the basics of the tutorial.

---
+++ Enumerations/Enumerations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enumerations
+{
+    public enum Days
+    {
+        Monday, Tuesday, Wednesday, Thursday,
+        Friday, Saturday, Sunday
+    }
+
+    public enum Numbers
+    {
+        One = 1, Two = 2, Seven = 7, Eight
+    }
+
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine((int)Days.Monday);
+            Console.WriteLine((Days)0);
+            Console.WriteLine((int)Numbers.Seven);
+            Console.WriteLine((int)Numbers.Eight);
+            //Get enums as strings
+            string[] dayStrings = Enum.GetNames(typeof(Days));
+            foreach (string ds in dayStrings)
+                Console.WriteLine(ds);
+        }
+    }
+}
