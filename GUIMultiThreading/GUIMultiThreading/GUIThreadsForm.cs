94ab7c91db35cb41d9945b1802364450f515f094
Cook <carsonjamescook@gmail.com>
2018-04-12 12:58:02 -0700

    GUIMultiThreading doesn't work yet.

---
+++ GUIMultiThreading/GUIMultiThreading/GUIThreadsForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GUIMultiThreading
+{
+    public partial class GUIThreadsForm : Form
+    {
+        public GUIThreadsForm()
+        {
+            InitializeComponent();
+        }
+
+        // My Code
+
+        // random letters
+        private RandomLetters letter1;
+        private RandomLetters letter2;
+        private RandomLetters letter3;
+
+        // labels
+        Label thread1Label = new Label();
+
+        private void GUIThreadsForm_Load(object sender, EventArgs e)
+        {
+            // create first thread
+            letter1 = new RandomLetters(thread1Label);
+            Thread thread1 = new Thread(new ThreadStart(letter1.GenerateRandomCharacters));
+            thread1.Name = "Thread 1";
+            thread1.Start();
+        }
+
+        private void GUIThreadsForm_FormClosing(object sender, FormClosedEventArgs e)
+        {
+            System.Environment.Exit(System.Environment.ExitCode);
+        }
+    }
+}
---
+++ GUIMultiThreading/GUIMultiThreading/RandomLetters.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GUIMultiThreading
+{
+    class RandomLetters
+    {
+        // used for random characters
+        private static Random generator = new Random();
+        // true if thread is suspended
+        private bool suspended = false;
+        // Label to display output
+        private Label output;
+        // name of ucrrent thread
+        private string threadName;
+
+        public RandomLetters(Label label)
+        {
+            output = label;
+        }
+
+        // delegate to be used for Invoke()
+        private delegate void DisplayDelegate(char displayChar);
+
+        // set Label's Text property
+        private void DisplayCharacter(char displayChar)
+        {
+            output.Text = threadName + ": " + displayChar;
+        }
+
+        public void GenerateRandomCharacters()
+        {
+            threadName = Thread.CurrentThread.Name;
+
+            // terminated from outside
+            while (true)
+            {
+                Thread.Sleep(generator.Next(1001));
+
+                lock(this)
+                {
+                    while (suspended)
+                    {
+                        Monitor.Wait(this);
+                    }
+                }
+
+                char displayChar = (char)(generator.Next(26) + 65);
+                output.Invoke(new DisplayDelegate(DisplayCharacter), new object[] { displayChar });
+            }
+        }
+
+        public void Toggle()
+        {
+            suspended = !suspended;
+            output.BackColor = suspended ? Color.Red : Color.LightGreen;
+
+            lock(this)
+            {
+                if (!suspended)
+                    Monitor.Pulse(this);
+            }
+        }
+    }
+}
