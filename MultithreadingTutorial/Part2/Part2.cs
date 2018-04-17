49f6a67e2b885f1e70162c5c30f492da61aba547
Cook <carsonjamescook@gmail.com>
2018-04-16 17:02:13 -0700

    Did more stuff for threading.

--- MultithreadingTutorial/Part2/Part2.cs
+++ MultithreadingTutorial/Part2/Part2.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Part2
 {
-    class Program
+    class Part2
     {
+        const int LOOPS = 5;
+        const int RANGE = 10;
+        static Random rand = new Random();
         static void Main(string[] args)
         {
+            Thread t = new Thread(LoopAndPrintRandom);
+            Thread.CurrentThread.Name = "Thread 1";
+            t.Name = "Thread 2";
+            t.Start();
+            LoopAndPrintRandom();
+        }
+
+        static void LoopAndPrintRandom()
+        {
+            int temp;
+            for (int i = 0; i < LOOPS; i++)
+            {
+                temp = rand.Next(RANGE);
+                Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, temp);
+            }
         }
     }
 }
