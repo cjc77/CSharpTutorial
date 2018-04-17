35a159e6ca94ea3210937a5bbf79601664fad8fd
Cook <carsonjamescook@gmail.com>
2018-04-17 10:01:55 -0700

    More threading.

--- MultithreadingTutorial/CapturedVariables/CapturedVariables.cs
+++ MultithreadingTutorial/CapturedVariables/CapturedVariables.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CapturedVariables
 {
-    class Program
+    class CapturedVariables
     {
+        static int loops = 10;
+        static int sleepTime = 2000;
         static void Main(string[] args)
         {
+            Mistake();
+            Thread.Sleep(sleepTime);
+            Console.WriteLine("\n********");
+            Fixed();
+            Thread.Sleep(sleepTime);
+            Console.WriteLine("\n********");
+        }
+
+        static void Mistake()
+        {
+            for (int i = 0; i < loops; i++)
+                new Thread(() => Console.Write(i)).Start();
+        }
+
+        static void Fixed()
+        {
+            int temp;
+            for (int i = 0; i < loops; i++)
+            {
+                temp = i;
+                new Thread(() => Console.Write(temp)).Start();
+            }
         }
     }
 }
