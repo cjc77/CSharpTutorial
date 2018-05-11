dbc212f172ab20e426402dcfa357db0b872c7677
Cook <carsonjamescook@gmail.com>
2018-05-10 18:00:08 -0700

    Played with text box a bit more.

---
+++ MultithreadingTutorialPart2/MultiThreadedTextBox/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MultiThreadedTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.threadControl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // threadControl
+            // 
+            this.threadControl.Location = new System.Drawing.Point(94, 63);
+            this.threadControl.Name = "threadControl";
+            this.threadControl.Size = new System.Drawing.Size(100, 25);
+            this.threadControl.TabIndex = 1;
+            this.threadControl.Text = "Thread Control";
+            this.threadControl.UseVisualStyleBackColor = true;
+            this.threadControl.MouseClick += new System.Windows.Forms.MouseEventHandler(this.threadControl_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.threadControl);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button threadControl;
+    }
+}
+
---
+++ MultithreadingTutorialPart2/MultiThreadedTextBox/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThreadedTextBox
+{
+    public partial class Form1 : Form
+    {
+        static Thread worker;
+        bool cont = true;
+        ManualResetEvent signalEvent = new ManualResetEvent(false);
+        public Form1()
+        {
+            InitializeComponent();
+            worker = new Thread(Work);
+            worker.Start();
+        }
+
+        void Work()
+        {
+            Random rand = new Random();
+            int curr;
+            while (true)
+            {
+                Thread.Sleep(rand.Next(1000, 2000));
+                if (cont)
+                {
+                    curr = rand.Next(1000, 2000);
+                    UpdateMessage(curr.ToString());
+                }
+                else
+                {
+                    signalEvent.WaitOne();
+                    signalEvent.Reset();
+                }
+            }
+        }
+
+        void UpdateMessage(string message)
+        {
+            Action action = () => _MessageAction(message);
+            this.BeginInvoke(action);
+        }
+
+        void _MessageAction(string message)
+        {
+            textBox1.Text = message;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void threadControl_MouseClick(object sender, MouseEventArgs e)
+        {
+            cont = !cont;
+            if (cont)
+            {
+                textBox1.Text = "pulsing";
+                signalEvent.Set();
+            }
+        }
+    }
+}
